d372525784f61a10efa045dc7dbf16d1bb0bd48c
sishuqun <sisq@8531.cn>
2018-07-02 16:03:51 +0800

    修复空指针问题

--- passport/src/main/java/com/zjrb/passport/ZbConfig.java
+++ passport/src/main/java/com/zjrb/passport/ZbConfig.java
@@ -2,6 +2,7 @@ package com.zjrb.passport;
 
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 
 import com.zjrb.passport.constant.ZbConstants;
 
@@ -21,11 +22,19 @@ final class ZbConfig {
     public ZbConfig() {}
 
     public ZbConfig(Context context) {
-        ApplicationInfo info = context.getApplicationInfo();
-        appId = info.metaData.getInt(ZbConstants.META_ID);
-        appKey = info.metaData.getString(ZbConstants.META_KEY);
-        appSecret = info.metaData.getString(ZbConstants.META_SECRET);
-        envType = info.metaData.getInt(ZbConstants.META_ENV);
+        ApplicationInfo info = null;
+        try {
+            info = context.getPackageManager()
+                    .getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (info != null) {
+            appId = info.metaData.getInt(ZbConstants.META_ID);
+            appKey = info.metaData.getString(ZbConstants.META_KEY);
+            appSecret = info.metaData.getString(ZbConstants.META_SECRET);
+            envType = info.metaData.getInt(ZbConstants.META_ENV);
+        }
     }
 
     public int getAppId() {
