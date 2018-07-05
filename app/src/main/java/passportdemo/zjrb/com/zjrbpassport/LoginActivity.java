fbbf297a51ca6ae43096b9f1d59aec1ea4b59e04
Chen.H <864514196@qq.com>
2018-07-05 16:42:50 +0800

    登录和快速登录界面

---
+++ app/src/main/java/passportdemo/zjrb/com/zjrbpassport/LoginActivity.java
@@ -0,0 +1,20 @@
+package passportdemo.zjrb.com.zjrbpassport;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Function: LoginActivity
+ * <p>
+ * Author: chen.h
+ * Date: 2018/7/5
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
