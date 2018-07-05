a7624a358a3401a3d7c078e2bdf1ac0986f7e626
Chen.H <864514196@qq.com>
2018-07-05 16:44:05 +0800

    绑定和修改手机界面

---
+++ app/src/main/java/passportdemo/zjrb/com/zjrbpassport/UserInfoActivity.java
@@ -0,0 +1,19 @@
+package passportdemo.zjrb.com.zjrbpassport;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Function: UserInfoActivity
+ * <p>
+ * Author: chen.h
+ * Date: 2018/7/5
+ */
+public class UserInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
