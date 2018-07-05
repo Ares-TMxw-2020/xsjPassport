196e999d1652354644e83c915dcde4376ae9774b
Chen.H <864514196@qq.com>
2018-07-05 09:05:31 +0800

    先实现功能

---
+++ passport/src/main/java/com/zjrb/passport/BaseData.java
@@ -0,0 +1,12 @@
+package com.zjrb.passport;
+
+/**
+ * Function: BaseData
+ * <p>
+ * Author: chen.h
+ * Date: 2018/7/4
+ */
+public class BaseData {
+    public int code;
+    public String message;
+}
--- passport/src/main/java/com/zjrb/passport/NetWork.java
+++ passport/src/main/java/com/zjrb/passport/NetWork.java
@@ -18,6 +18,9 @@ import com.zjrb.passport.net.Response;
 import com.zjrb.passport.net.ZbHttpClient;
 import com.zjrb.passport.net.util.EncryptUtil;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -42,32 +45,102 @@ public class NetWork {
 
     }
 
+    private BaseData json(String jsonString) {
+        JSONObject jsonObject;
+        BaseData data = null;
+        try {
+            jsonObject = new JSONObject(jsonString);
+            data = new BaseData();
+            data.code = jsonObject.getInt("code");
+            data.message = jsonObject.getString("message");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
     public void sendRegisterCaptcha(String phoneNumber, final ZbCaptchaListener listener) {
         ParamsBuilder builder = new ParamsBuilder(ApiManager.EndPoint.SMS_SEND_REGISTER_TOKEN).add("phone_number",
                                                                                                    phoneNumber);
         client.newCall(getRequest(builder)).enqueue(new CallBack() {
             @Override
             public void onSuccess(Response response) throws IOException {
-                listener.onSuccess();
+                BaseData data = json(response.body().string());
+                if (data.code == StatusCode.SUCCESS) {
+                    listener.onSuccess();
+                } else {
+                    listener.onFailure(data.code, data.message);
+                }
             }
 
             @Override
             public void onFail(Request call, IOException e) {
-                listener.onFailure(0, "");
+                listener.onFailure(-1, StatusCode.getMessage(-1));
             }
         });
     }
 
-    public void sendLoginCaptcha(String phoneNumber, ZbCaptchaListener listener) {
+    public void sendLoginCaptcha(String phoneNumber, final ZbCaptchaListener listener) {
+        ParamsBuilder builder = new ParamsBuilder(ApiManager.EndPoint.SMS_SEND_LOGIN_TOKEN).add("phone_number",
+                                                                                                phoneNumber);
+        client.newCall(getRequest(builder)).enqueue(new CallBack() {
+            @Override
+            public void onSuccess(Response response) throws IOException {
+                BaseData data = json(response.body().string());
+                if (data.code == StatusCode.SUCCESS) {
+                    listener.onSuccess();
+                } else {
+                    listener.onFailure(data.code, data.message);
+                }
+            }
 
+            @Override
+            public void onFail(Request call, IOException e) {
+                listener.onFailure(-1, StatusCode.getMessage(-1));
+            }
+        });
     }
 
-    public void sendRetrieveCaptcha(String phoneNumber, ZbCaptchaListener listener) {
+    public void sendRetrieveCaptcha(String phoneNumber, final ZbCaptchaListener listener) {
+        ParamsBuilder builder = new ParamsBuilder(ApiManager.EndPoint.SMS_SEND_RESET_TOKEN).add("phone_number",
+                                                                                                phoneNumber);
+        client.newCall(getRequest(builder)).enqueue(new CallBack() {
+            @Override
+            public void onSuccess(Response response) throws IOException {
+                BaseData data = json(response.body().string());
+                if (data.code == StatusCode.SUCCESS) {
+                    listener.onSuccess();
+                } else {
+                    listener.onFailure(data.code, data.message);
+                }
+            }
 
+            @Override
+            public void onFail(Request call, IOException e) {
+                listener.onFailure(-1, StatusCode.getMessage(-1));
+            }
+        });
     }
 
-    public void sendBindCaptcha(String phoneNumber, ZbCaptchaListener listener) {
+    public void sendBindCaptcha(String phoneNumber, final ZbCaptchaListener listener) {
+        ParamsBuilder builder = new ParamsBuilder(ApiManager.EndPoint.SMS_SEND_BINDING_TOKEN).add("phone_number",
+                                                                                                  phoneNumber);
+        client.newCall(getRequest(builder)).enqueue(new CallBack() {
+            @Override
+            public void onSuccess(Response response) throws IOException {
+                BaseData data = json(response.body().string());
+                if (data.code == StatusCode.SUCCESS) {
+                    listener.onSuccess();
+                } else {
+                    listener.onFailure(data.code, data.message);
+                }
+            }
 
+            @Override
+            public void onFail(Request call, IOException e) {
+                listener.onFailure(-1, StatusCode.getMessage(-1));
+            }
+        });
     }
 
     public void register(String phoneNumber, String password, String captcha, ZbRegisterListener listener) {
