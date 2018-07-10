f8c6e72efb879c3e96ba79974f285ba6cc2be5f6
sishuqun <sisq@8531.cn>
2018-07-10 10:25:06 +0800

    添加手机号判断

--- app/src/main/java/passportdemo/zjrb/com/zjrbpassport/utils/ZbUtil.java
+++ app/src/main/java/passportdemo/zjrb/com/zjrbpassport/utils/ZbUtil.java
@@ -2,9 +2,13 @@ package passportdemo.zjrb.com.zjrbpassport.utils;
 
 import android.app.Application;
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.WindowManager;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Date: 2018/7/9 下午3:30
@@ -45,6 +49,38 @@ public class ZbUtil {
     }
 
     /**
+     * 判断是否是纯数字
+     * @param str
+     * @return
+     */
+    public static boolean isNum(String str) {
+        Pattern pattern = Pattern.compile("[0-9]*");
+        Matcher matcher = pattern.matcher(str);
+        if (!matcher.matches()) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 判断是否是手机号码
+     * @param mobiles
+     * @return
+     */
+    public static boolean isMobileNum(String mobiles) {
+        // 不为空,长度是11,数字类型
+        if (!TextUtils.isEmpty(mobiles) && isNum(mobiles) && mobiles.length() == 11) {
+            Pattern p = Pattern.compile("[1]\\d{10}"); // 1开头,匹配数字
+            Matcher m = p.matcher(mobiles);
+            return m.matches();
+        } else {
+            return false;
+        }
+
+    }
+
+
+    /**
      * dip转换px
      */
     public static int dip2px(float dip) {
@@ -73,7 +109,5 @@ public class ZbUtil {
      */
     public static float px2sp(float pxVal) {
         return (pxVal / getContext().getResources().getDisplayMetrics().scaledDensity);
-
-
     }
 }
