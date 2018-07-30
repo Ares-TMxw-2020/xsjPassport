87f2e1a08472ae7117d0e1f0af66e41a0f139aa3
sishuqun <sisq@8531.cn>
2018-07-30 17:37:08 +0800

    找回密码时,在发送验证码时增加手机号是否注册浙报通行证的校验

--- app/src/main/java/com/zhejiangdaily/presenters/FindPassWordPresenterImpl.java
+++ app/src/main/java/com/zhejiangdaily/presenters/FindPassWordPresenterImpl.java
@@ -8,10 +8,13 @@ import com.zhejiangdaily.contracts.FindPasswordContract;
 import com.zhejiangdaily.utils.ToastUtil;
 import com.zhejiangdaily.utils.ZbUtil;
 import com.zhejiangdaily.views.activities.FindNewPassWordActivity;
+import com.zhejiangdaily.views.activities.RegisterActvity;
+import com.zhejiangdaily.views.dialogs.TipDialog;
 import com.zjrb.passport.ZbPassport;
 import com.zjrb.passport.constant.ZbConstants;
 import com.zjrb.passport.listener.ZbCaptchaSendListener;
 import com.zjrb.passport.listener.ZbCaptchaVerifyListener;
+import com.zjrb.passport.listener.ZbCheckPhoneListener;
 
 /**
  * Date: 2018/7/10 下午5:39
@@ -28,12 +31,42 @@ public class FindPassWordPresenterImpl implements FindPasswordContract.Presenter
     }
 
     @Override
-    public void sendSms(String phoneNum) {
+    public void sendSms(final String phoneNum) {
         if (ZbUtil.isMobileNum(phoneNum)) {
-            ZbPassport.sendCaptcha(ZbConstants.Sms.FIND,phoneNum, new ZbCaptchaSendListener() {
+            ZbPassport.checkBindState(phoneNum, new ZbCheckPhoneListener() { // 发送验证码前先校验手机号是否注册过浙报通行证接口
                 @Override
-                public void onSuccess() {
+                public void onSuccess(boolean isBind) {
+                    if (isBind) {
+                        ZbPassport.sendCaptcha(ZbConstants.Sms.FIND, phoneNum, new ZbCaptchaSendListener() {
+                            @Override
+                            public void onSuccess() {
 
+                            }
+
+                            @Override
+                            public void onFailure(int errorCode, String errorMessage) {
+                                ToastUtil.show(errorMessage);
+                            }
+                        });
+                    } else {
+                        TipDialog tipDialog = new TipDialog(view.getIActivity());
+                        tipDialog.setTitle("提示")
+                                .setMessage("此手机号尚未注册，请先注册")
+                                .setLeft("取消")
+                                .setRight("注册")
+                                .setListener(new TipDialog.Listener() {
+                                    @Override
+                                    public void onLeft() {
+
+                                    }
+
+                                    @Override
+                                    public void onRight() {
+                                        view.getIActivity().startActivity(new Intent(view.getIActivity(), RegisterActvity.class));
+                                    }
+                                });
+                        tipDialog.show();
+                    }
                 }
 
                 @Override
