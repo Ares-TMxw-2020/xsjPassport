f06f483819b91916220246b06204632e041906ce
sishuqun <sisq@8531.cn>
2018-08-01 16:18:50 +0800

    用户注册时发送验证码增加手机号校验

--- app/src/main/java/com/zhejiangdaily/views/activities/RegisterActvity.java
+++ app/src/main/java/com/zhejiangdaily/views/activities/RegisterActvity.java
@@ -18,6 +18,7 @@ import com.zjrb.passport.ZbPassport;
 import com.zjrb.passport.constant.ErrorCode;
 import com.zjrb.passport.constant.ZbConstants;
 import com.zjrb.passport.listener.ZbCaptchaSendListener;
+import com.zjrb.passport.listener.ZbCheckPhoneListener;
 import com.zjrb.passport.listener.ZbRegisterListener;
 
 import butterknife.BindView;
@@ -73,8 +74,8 @@ public class RegisterActvity extends AppCompatActivity {
                     ToastUtil.show("请输入密码");
                     return;
                 } else if (passWord.length() < 6) {
-                        ToastUtil.show("密码长度小于6,请输入6到15位密码!");
-                        return;
+                    ToastUtil.show("密码长度小于6,请输入6到15位密码!");
+                    return;
                 }
 
                 ZbPassport.register(phoneNum, passWord, captcha, new ZbRegisterListener() {
@@ -89,32 +90,32 @@ public class RegisterActvity extends AppCompatActivity {
 
                     @Override
                     public void onFailure(int errorCode, String errorMessage) {
-                       if (errorCode == ErrorCode.ERROR_PHONE_REGISTERED) {
+                        if (errorCode == ErrorCode.ERROR_PHONE_REGISTERED) {
                             final ZBDialog dialog = new ZBDialog(RegisterActvity.this);
                             dialog.setBuilder(new ZBDialog.Builder().setTitle("提示")
-                                                                    .setMessage("此手机号已经存在,可直接登录")
-                                                                    .setLeftText("取消")
-                                                                    .setRightText("登录")
-                                                                    .setOnClickListener(new View.OnClickListener() {
-                                                                        @Override
-                                                                        public void onClick(View v) {
-                                                                            if (v.getId() == R.id.btn_right) {
-                                                                                Intent intent = new Intent(
-                                                                                        RegisterActvity.this,
-                                                                                        LoginActivity.class);
-                                                                                startActivity(intent);
-                                                                            }
-                                                                            if (v.getId() == R.id.btn_left) {
-                                                                                if (dialog.isShowing()) {
-                                                                                    dialog.dismiss();
-                                                                                }
-                                                                            }
-                                                                        }
-                                                                    }));
+                                    .setMessage("此手机号已经存在,可直接登录")
+                                    .setLeftText("取消")
+                                    .setRightText("登录")
+                                    .setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            if (v.getId() == R.id.btn_right) {
+                                                Intent intent = new Intent(
+                                                        RegisterActvity.this,
+                                                        LoginActivity.class);
+                                                startActivity(intent);
+                                            }
+                                            if (v.getId() == R.id.btn_left) {
+                                                if (dialog.isShowing()) {
+                                                    dialog.dismiss();
+                                                }
+                                            }
+                                        }
+                                    }));
                             dialog.show();
                         } else {
-                           ToastUtil.show(errorMessage);
-                       }
+                            ToastUtil.show(errorMessage);
+                        }
                     }
                 });
                 break;
@@ -125,7 +126,47 @@ public class RegisterActvity extends AppCompatActivity {
             case R.id.tv_send:
                 // 校验手机号
                 if (ZbUtil.isMobileNum(phoneNum)) {
-                    ZbPassport.sendCaptcha(ZbConstants.Sms.REGISTER, phoneNum, new ZbCaptchaSendListener() {
+                    checkBind(phoneNum); // 发送验证码时校验是否已经注册浙报通行证
+                } else {
+                    if (TextUtils.isEmpty(phoneNum)) {
+                        ToastUtil.show("请输入手机号");
+                    } else {
+                        ToastUtil.show("非手机号格式");
+                    }
+                }
+                break;
+        }
+    }
+
+    private void checkBind(final String phone) {
+        ZbPassport.checkBindState(phone, new ZbCheckPhoneListener() {
+            @Override
+            public void onSuccess(boolean isBind) {
+                if (isBind) {
+                    final ZBDialog dialog = new ZBDialog(RegisterActvity.this);
+                    dialog.setBuilder(new ZBDialog.Builder().setTitle("提示")
+                            .setMessage("此手机号已经存在,可直接登录")
+                            .setLeftText("取消")
+                            .setRightText("登录")
+                            .setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    if (v.getId() == R.id.btn_right) {
+                                        Intent intent = new Intent(
+                                                RegisterActvity.this,
+                                                LoginActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    if (v.getId() == R.id.btn_left) {
+                                        if (dialog.isShowing()) {
+                                            dialog.dismiss();
+                                        }
+                                    }
+                                }
+                            }));
+                    dialog.show();
+                } else { // 未绑定过的手机号发送验证码
+                    ZbPassport.sendCaptcha(ZbConstants.Sms.REGISTER, phone, new ZbCaptchaSendListener() {
                         @Override
                         public void onSuccess() {
                             ToastUtil.show("下发注册短信验证码接口 success");
@@ -136,17 +177,14 @@ public class RegisterActvity extends AppCompatActivity {
                             ToastUtil.show(errorMessage);
                         }
                     });
-                } else {
-                    if (TextUtils.isEmpty(phoneNum)) {
-                        ToastUtil.show("请输入手机号");
-                    } else {
-                        ToastUtil.show("非手机号格式");
-                    }
                 }
+            }
 
-                break;
-        }
+            @Override
+            public void onFailure(int errorCode, String errorMessage) {
+                ToastUtil.show(errorMessage);
+            }
+        });
+
     }
-
-
 }
